acb31057aafdd07800eb59a942446dcf0cbedd19
supuna97 <supunnanayakkara01@gmail.com>
2023-10-06 19:36:30 +0530

    Add cors to routes

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
+const cors = require('cors');
 require("dotenv").config();
 
 const userRoute = require("./src/routes/api_v1");
 
 //middleware
 app.use(express.json());
+app.use(cors()); // Allow all origins (not recommended for production)
 
 //routes
 app.use("/v1", userRoute);
